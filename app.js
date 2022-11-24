40da6c0a1e724fbb023b8cd5d700cb7c3dd2dbb4
ryusei <117654409+ryusei00734@users.noreply.github.com>
2022-11-24 22:22:05 +0900

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-for (let i = 0;  i<=50;  i++) {
+for (let i = 1;  i<=50;  i++) {
     console.log(i);
 
     if(i % 10 === 0) {
         console.log(`今${i}回ループしました。`)
-    }else if (i % 4 === 0) {
+    }if (i % 4 === 0) {
         console.log(`4で割れる数字です。` + [i])
     }
 }
